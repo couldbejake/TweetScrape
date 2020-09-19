7e215958c7df48a152ecec0238d1ceb2506cfc2a
Jake Nelson <couldbejake@gmail.com>
2020-09-19 14:46:23 +0100

    Update basic_scraper.py

--- basic_scraper.py
+++ basic_scraper.py
@@ -1,5 +1,3 @@
-# run via cron every 15 minutes
-
 import os
 import time
 import json
@@ -8,17 +6,28 @@ import random
 import pickle
 import subprocess
 
+
 def dump_json(obj):
     with open('data.json', 'w') as outfile:
         json.dump(obj, outfile)
 
-def generate_token():
-    user_agent = f'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'
-    guest_token = subprocess.check_output("curl -skL https://twitter.com/ -H {user_agents[1]} --compressed | grep -o 'gt=[0-9]*' | sed s.gt=..", shell=True, text=True).strip()
-    return guest_token
+
+def get_twitter_gt():
+    headers = {
+
+        # Token 1
+        #'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANk3DgEAAAAAB0pZx4xjgXBOoalj%2FRbagurxD2M%3DG8634UVlBud8LrLG4nGo7FpN2RCO2xul5BuPKHuejUAV14O0KG',
+
+        # Token 2
+        'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+    }
+
+    response = requests.post('https://api.twitter.com/1.1/guest/activate.json', headers=headers)
+    guest_token = (response.json()['guest_token'])
+
+    return guest_token;
 
 def twitter_data(search_term, since, until):
-    
     url = 'https://api.twitter.com/2/search/adaptive.json'
 
     params = {
@@ -31,35 +40,34 @@ def twitter_data(search_term, since, until):
         'include_can_dm': 1,
         'include_can_media_tag': 1,
         'skip_status': 1,
-        'cards_platform':'Web-12',
+        'cards_platform': 'Web-12',
         'include_cards': 1,
-        'include_ext_alt_text':'true',
-        'include_quote_count':'true',
+        'include_ext_alt_text': 'true',
+        'include_quote_count': 'true',
         'include_reply_count': 1,
-        'tweet_mode':'extended',
-        'include_entities':'true',
-        'include_user_entities':'true',
-        'include_ext_media_color':'true',
-        'include_ext_media_availability':'true',
-        'send_error_codes':'true',
-        'simple_quoted_tweet':'true',
+        'tweet_mode': 'extended',
+        'include_entities': 'true',
+        'include_user_entities': 'true',
+        'include_ext_media_color': 'true',
+        'include_ext_media_availability': 'true',
+        'send_error_codes': 'true',
+        'simple_quoted_tweet': 'true',
         'q': str(search_term) + " since:" + str(since) + " until:" + str(until),
-        'count':'50',
-        'query_source':'typed_query',
+        'count': '50',
+        'query_source': 'typed_query',
         'pc': 1,
         'spelling_corrections': 'false',
-        'ext':'mediaStats%2ChighlightedLabel',
+        'ext': 'mediaStats%2ChighlightedLabel',
     }
 
     headers = {
         'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
-        'x-guest-token': generate_token()
+        'x-guest-token': get_twitter_gt()
     }
 
     res = requests.get(url, params=params, headers=headers)
 
     json_data = res.json()
-
 
     tw_count = len(json_data['globalObjects']['tweets'])
 
@@ -67,3 +75,10 @@ def twitter_data(search_term, since, until):
 
 
 twitter_data("$fb", '2015-01-01', '2015-01-06')
+
+
+
+
+
+
+
